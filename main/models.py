608ed11e9837252304cba4f6d46db2d7099d7cd6
H3CT1C <gohtingqi1997@hotmail.com>
2021-03-02 14:01:05 +0800

    it works

--- main/models.py
+++ main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Town(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+
+
+class LevelType(models.Model):
+    storey_range = models.CharField(max_length=255, primary_key=True)
+
+
+class FlatType(models.Model):
+    no_of_rooms = models.IntegerField(max_length=32, primary_key=True)
+
+
+class BlockAddress(models.Model):
+    id = models.IntegerField(max_length=32,primary_key=True)
+    block = models.CharField(max_length=255)
+    street_name = models.CharField(max_length=255)
+    town_name_id= models.ForeignKey(Town, on_delete=models.CASCADE)
+    coordinate = models.CharField(max_length=32)
+
+
+class Room(models.Model):
+    id = models.IntegerField(max_length=32, primary_key=True)
+    flat_type_id= models.ForeignKey(FlatType,on_delete=models.CASCADE)
+    level_type_id= models.ForeignKey(LevelType, on_delete=models.CASCADE)
+    block_address_id = models.ForeignKey(BlockAddress, on_delete=models.CASCADE)
+    town_name_id= models.ForeignKey(Town, on_delete=models.CASCADE)
+    resale_prices = models.IntegerField(max_length=32)
+    remaining_lease = models.CharField(max_length=255)
+
+
+
+
