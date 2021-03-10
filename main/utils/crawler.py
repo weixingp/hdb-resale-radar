86756eed652181d22b450b8e8887f6d388f446d8
datmanlyman <jeewern2@gmail.com>
2021-03-10 20:13:47 +0800

    crawler - webpage within webpage

--- main/utils/crawler.py
+++ main/utils/crawler.py
@@ -2,8 +2,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from itertools import chain
-
+import json
 import requests
+from urllib import request
 from bs4 import BeautifulSoup
 
 
@@ -11,26 +12,64 @@ def getdata(url):
     r = requests.get(url)
     return r.text
 
-URL = 'https://mothership.sg/2021/03/environmental-control-coordinator-sanitation-regime/' #insert url here
-page = requests.get(URL)
-soup = BeautifulSoup(page.text, 'html.parser')
+# function needed to deal with nested dictionary of json
+def findkeys(node, kv):
+    if isinstance(node, list):
+        for i in node:
+            for x in findkeys(i, kv):
+                yield x
+    elif isinstance(node, dict):
+        if kv in node:
+            yield node[kv]
+        for j in node.values():
+            for x in findkeys(j, kv):
+                yield x
 
+#loads the url, puts into beautiful soup and turns into a json dictionary
 
-for item2 in soup.find_all('figure', class_='featured-image'): #for image
-    image = item2.img['src']
-    print(image)
+urlList = ['https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyDuQi378YmI9zlQYBgTNV9ZH50GsiXBXsI&cx=007628173177510673425%3A1wke5kj-ez8&q=allintitle%3A%20hdb&num=10&start=0',
+           'https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyDuQi378YmI9zlQYBgTNV9ZH50GsiXBXsI&cx=007628173177510673425%3A1wke5kj-ez8&q=allintitle%3A%20hdb&num=10&start=22',
+           'https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyDuQi378YmI9zlQYBgTNV9ZH50GsiXBXsI&cx=007628173177510673425%3A1wke5kj-ez8&q=allintitle%3A%20hdb&num=10&start=44',
+           'https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyDuQi378YmI9zlQYBgTNV9ZH50GsiXBXsI&cx=007628173177510673425%3A1wke5kj-ez8&q=allintitle%3A%20hdb&num=10&start=55',
+           'https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyDuQi378YmI9zlQYBgTNV9ZH50GsiXBXsI&cx=007628173177510673425%3A1wke5kj-ez8&q=allintitle%3A%20hdb&num=10&start=77']
 
-empty_list = []
+k = 0
+urls = []
 
-for item2 in soup.find_all('title'): #for title
-    print(item2.string)
+for i in urlList:
+    html = request.urlopen(urlList[k]).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    site_json = json.loads(soup.text)
+    urls.append(list(findkeys(site_json, 'link')))
+    k += 1
 
-for item2 in soup.find_all('p'): #for summary
-    if item2.string is not None:
-        if len(item2.string) > 100:
-            y = item2.string.split()[:]
-            empty_list.append(y)
+j = 0
 
-final = list(chain.from_iterable(empty_list))
-final = final[:100]
-print(' '.join(final) + "...")
+for i in urls:
+    l = 0
+    for m in urls[j]:
+        URL = urls[j][l]
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        for item2 in soup.find_all('figure', class_='featured-image'): #for image
+            image = item2.img['src']
+            print(image)
+
+        empty_list = []
+
+        for item2 in soup.find_all('title'): #for title
+            print(item2.string)
+
+        for item2 in soup.find_all('p'): #for summary
+            if item2.string is not None:
+                if len(item2.string) > 100:
+                    y = item2.string.split()[:]
+                    empty_list.append(y)
+
+        final = list(chain.from_iterable(empty_list))
+        final = final[:100]
+        print(' '.join(final) + "...")
+        print()
+        l += 1
+    j += 1
